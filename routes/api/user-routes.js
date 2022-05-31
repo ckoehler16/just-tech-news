1bcd247bdbd0473e83d016d2b2096b153d57c07a
Koehler <ckoehler16@gmail.com>
2022-05-31 18:57:27 -0400

    created login route

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -54,6 +54,31 @@ router.post('/', (req, res) => {
         });
 });
 
+// POST /login route
+router.post('/login', (req, res) => {
+    // Expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        res.json({ user: dbUserData, message: 'You are logged in!' });
+    });
+});
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     // Expects {username: 'Lernantine', email: 'lernantino@gmail.com', password: 'password1234'}
